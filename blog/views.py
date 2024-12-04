6ea5867217294d329b6e134ede72bbef98356d86
komarbv211 <andrijkoslak@gmail.com>
2024-12-04 18:17:50 +0200

    Add project

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from urllib import request
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request,"home.html")
+
+
+def contactpage(request):
+    return render(request, "contact.html")
